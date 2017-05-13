d28baae84f3044fb7d9f631825c3a146ecd02404
alexandervollovec <alex_v@gmx.at>
2017-05-13 18:54:16 +0200

    Weitere Entities hinzugefuegt. Zugehoerige Facades erstellt.

--- src/main/java/at/gv/ooe/ietservice/business/InitBean.java
+++ src/main/java/at/gv/ooe/ietservice/business/InitBean.java
@@ -1,5 +1,8 @@
 package at.gv.ooe.ietservice.business;
 
+import at.gv.ooe.ietservice.entity.ConfigurationItem;
+import at.gv.ooe.ietservice.entity.Department;
+import at.gv.ooe.ietservice.entity.DiaryEntry;
 import at.gv.ooe.ietservice.entity.Ticket;
 
 import javax.annotation.PostConstruct;
@@ -16,13 +19,32 @@ public class InitBean {
 
     @Inject
     TicketFacade ticketFacade;
+    @Inject
+    UserFacade userFacade;
+    @Inject
+    DiaryEntry diaryEntryFacade;
+    @Inject
+    DepartmentFacade departmentFacade;
+    @Inject
+    ConfigurationItemFacade configurationItemFacade;
 
     @PostConstruct
     private void init() {
-        ticketFacade.save(new Ticket("Drucker kaputt"));
-        ticketFacade.save(new Ticket("Monitor zu klein"));
-        ticketFacade.save(new Ticket("Strom weg"));
-        ticketFacade.save(new Ticket("Sessel fehlt"));
+        Ticket ticket1 = new Ticket("Akku defekt", "10.05.2017", "12.05.2017", "", "Aktiv", "Huber", "Mizi", "Dringend", "");
+        ticket1.getDiaryEntries().add(new DiaryEntry("10.05.2017 15:30", "Huber", "Akku muss ausgetauscht werden."));
+        ticket1.getDiaryEntries().add(new DiaryEntry("10.05.2017 16:15", "Fischer", "Warten auf Anlieferung"));
+        ticket1.getDiaryEntries().add(new DiaryEntry("10.05.2017 15:30", "Huber", "Akku doch nicht defekt. Geraet muss getauscht werden."));
+        ticketFacade.save(ticket1);
+        ticketFacade.save(new Ticket("Toner nicht geliefert", "08.05.2017", "11.05.2017", "", "Aktiv", "Mayr", "Gundula", "Nicht Dringend", ""));
+        ticketFacade.save(new Ticket("Kein Strom", "04.05.2017", "05.05.2017", "", "Ausgesetzt", "Leeb", "Heribert", "Dringend", ""));
+        ticketFacade.save(new Ticket("Computer zu langsam", "12.05.2017", "", "", "Aktiv", "Hofer", "Hugo", "Sehr dringend", ""));
+        Department dept1 = new Department("Wohnbaufoerderung");
+        Department dept2 = new Department("Naturschutz");
+        Department dept3 = new Department("Wirtschaft");
+        departmentFacade.save(dept1);
+        departmentFacade.save(dept2);
+        departmentFacade.save(dept3);
+
     }
 
 }
--- src/main/java/at/gv/ooe/ietservice/entity/Ticket.java
+++ src/main/java/at/gv/ooe/ietservice/entity/Ticket.java
@@ -1,6 +1,8 @@
 package at.gv.ooe.ietservice.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by alexandervollovec on 07/05/2017.
@@ -10,17 +12,39 @@ import javax.persistence.*;
 })
 @Entity
 public class Ticket {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
-    String description;
+    private String description;
+    private String startDate;
+    private String answerDate;
+    private String closedDate;
+    private String status;
+    private String specialistName;
+    private String problemUserName;
+    private String priority;
+    private String solution;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DiaryEntry> diaryEntries = diaryEntries = new ArrayList<>();
 
     public Ticket() {
     }
 
-    public Ticket(String description) {
+    public Ticket(String description, String startDate, String answerDate,
+                  String closedDate, String status, String specialistName,
+                  String problemUserName, String priority, String solution) {
         this.description = description;
+        this.setStartDate(startDate);
+        this.setAnswerDate(answerDate);
+        this.setClosedDate(closedDate);
+        this.setStatus(status);
+        this.setSpecialistName(specialistName);
+        this.setProblemUserName(problemUserName);
+        this.setPriority(priority);
+        this.setSolution(solution);
+        diaryEntries = new ArrayList<>();
     }
 
     public Long getId() {
@@ -33,6 +57,78 @@ public class Ticket {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getAnswerDate() {
+        return answerDate;
+    }
+
+    public void setAnswerDate(String answerDate) {
+        this.answerDate = answerDate;
+    }
+
+    public String getClosedDate() {
+        return closedDate;
+    }
+
+    public void setClosedDate(String closedDate) {
+        this.closedDate = closedDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSpecialistName() {
+        return specialistName;
+    }
+
+    public void setSpecialistName(String specialistName) {
+        this.specialistName = specialistName;
+    }
+
+    public String getProblemUserName() {
+        return problemUserName;
+    }
+
+    public void setProblemUserName(String problemUserName) {
+        this.problemUserName = problemUserName;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+    }
+
+    public List<DiaryEntry> getDiaryEntries() {
+        return diaryEntries;
+    }
+
+    public void setDiaryEntries(List<DiaryEntry> diaryEntries) {
+        this.diaryEntries = diaryEntries;
     }
 
     @Override
